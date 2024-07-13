29cc62a4e2ba4935fd7a9551280f1db7860b2cd0
goosebbeoms <seo980916@gmail.com>
2024-07-13 12:07:09 +0900

    234. HTTP 에러 다루기

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,29 +1,44 @@
 import { useState, useEffect } from 'react';
 
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFatching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     setAvailablePlaces(true);
     async function fetchPlaces() {
-      const response = fetch('http://localhost:8000/places');
-      const resData = response.json();
-      setAvailablePlaces(resData.places)
-      setAvailablePlaces(false)
+      try {
+        const response = await fetch('http://localhost:3000/places');
+        const resData = await response.json();
+        
+        if (!response.ok) {
+          throw new Error('Failed to fetch places');
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({message: error.message || 'Could not fetch places, please try again later'});
+      }
+
+      setIsFetching(false);
     }
 
     fetchPlaces()
   }, [])
 
+  if (error) {
+    return <Error title="An error occured!" message={error.message} />
+  }
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
-      isLoading={false}
+      isLoading={isFatching}
       loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
